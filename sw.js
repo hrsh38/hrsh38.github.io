e3a991e74dfae0f038afd41f04ab775c97728adb
Harsh Patel <harshp97@gmail.com>
2020-01-01 17:00:08 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2bb26611b8ec7e2aebf9.js"
+    "url": "webpack-runtime-9e2a55ce6ee2651c64ce.js"
   },
   {
     "url": "commons-ff88e6e2f41106d90892.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3fc13adb42d9381bded7dc204f109274"
+    "revision": "9c0d6dee04c9eab0994718960c8e79ec"
   },
   {
     "url": "manifest.webmanifest",
